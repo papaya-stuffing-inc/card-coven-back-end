0ff33359513c769c2bd772121adf24443c9d28d6
Cosmo Kent <cosmolightfoot@gmail.com>
2019-07-06 22:42:58 -0700

    added multiple sort field functionality, tests passing

--- test/unit/sortQuery.test.js
+++ test/unit/sortQuery.test.js
@@ -4,4 +4,10 @@ describe('sort query tests', () => {
   it('returns a sort query object', () => {
     expect(sortQuery('cmc,-1')).toEqual({ cmc: -1 });
   });
+  it('works with multiple search queries', () => {
+    expect(sortQuery('cmc,-1|prices,-1')).toEqual({
+      cmc: -1,
+      prices: -1
+    });
+  });
 });
