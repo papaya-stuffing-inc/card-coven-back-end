2a6fe24db030e7d10a5dd004c91834d2d5c0c6a0
Cosmo Kent <cosmolightfoot@gmail.com>
2019-07-18 16:05:10 -0700

    about to rework services into promises

--- server.js
+++ server.js
@@ -1,9 +1,19 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
+const initializeCardData = require('./lib/services/initialize-card-data');
 const app = require('./lib/app');
+const fs = require('fs');
+
+const cardDataExists = fs.existsSync('./card-data/scryfall/card-data.json');
+
+if(!cardDataExists) {
+  //eslint-disable-next-line no-console
+  console.log('No card data exists. Initializing card data now...');
+  initializeCardData();
+}
+
 
 const PORT = process.env.PORT || 7891;
-
 app.listen(PORT, () => {
 //eslint-disable-next-line no-console
   console.log(`You are listening to smoooth jazz on port ${PORT}`);
