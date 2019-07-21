6f04cd79e5e2fe191b925220a5122d97ddf7111b
Cosmo Kent <cosmolightfoot@gmail.com>
2019-07-20 18:57:27 -0700

    added transform cards fetch to init

--- TODO.md
+++ TODO.md
@@ -1,3 +1,6 @@
--fix card layout edge cases (transform, split, etc);
+-fix card layout edge cases (transform, double-faced token);
+  -make update/populate services fetch transform cards
+  -add transform cards prints to db
+  -populate transform cards on get request
 -refactor color query util to be more /elegant/
 -add search by rarity
--- server.js
+++ server.js
@@ -5,16 +5,23 @@ const app = require('./lib/app');
 const fs = require('fs');
 
 const cardDataExists = fs.existsSync('./card-data/scryfall/card-data.json');
+const PORT = process.env.PORT || 7891;
+
+function runServer() {
+  app.listen(PORT, () => {
+    //eslint-disable-next-line no-console
+    console.log(`You are listening to smoooth jazz on port ${PORT}`);
+  });
+}
 
 if(!cardDataExists) {
   //eslint-disable-next-line no-console
   console.log('No card data exists. Initializing card data now...');
-  initializeCardData();
+  initializeCardData(runServer);
+} else {
+  //eslint-disable-next-line no-console
+  console.log('Card Data Exists');
+  runServer();
 }
 
 
-const PORT = process.env.PORT || 7891;
-app.listen(PORT, () => {
-//eslint-disable-next-line no-console
-  console.log(`You are listening to smoooth jazz on port ${PORT}`);
-});
