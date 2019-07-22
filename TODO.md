bb1eb2d50bedfa97b655b8df6044adf394ae8991
Cosmo Kent <cosmolightfoot@gmail.com>
2019-07-22 16:45:22 -0700

    automatically updates cards at 4:30pm mon-thurs

--- TODO.md
+++ TODO.md
@@ -1,6 +1,7 @@
--fix card layout edge cases (transform, double-faced token);
+<!-- -fix card layout edge cases (transform, double-faced token);
   -make update/populate services fetch transform cards
   -add transform cards prints to db
-  -populate transform cards on get request
+  -populate transform cards on get request -->
 -refactor color query util to be more /elegant/
 -add search by rarity
+-refactor fetchTransformCards setInterval into a recursive function
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 require('dotenv').config();
 require('./lib/utils/connect')();
 const initializeCardData = require('./lib/services/initialize-card-data');
+const updateCardData = require('./lib/services/update-card-data');
 const app = require('./lib/app');
 const fs = require('fs');
+const schedule = require('node-schedule');
 
 const cardDataExists = fs.existsSync('./card-data/scryfall/card-data.json');
 const PORT = process.env.PORT || 7891;
@@ -11,6 +13,12 @@ function runServer() {
   app.listen(PORT, () => {
     //eslint-disable-next-line no-console
     console.log(`You are listening to smoooth jazz on port ${PORT}`);
+    //eslint-disable-next-line no-unused-vars
+    const scheduleUpdate = schedule.scheduleJob('0 30 16 * * 1,4', () => {
+      //eslint-disable-next-line no-console
+      console.log('Updating card data');
+      updateCardData();
+    });
   });
 }
 
