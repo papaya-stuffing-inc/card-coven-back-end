9e4223f2c013fbda8fa6c0855890ff03984b8f5d
Cosmo Kent <cosmolightfoot@gmail.com>
2019-07-24 16:19:02 -0700

    pre branch commit

---
+++ card-data/layouts.js
@@ -0,0 +1,17 @@
+const allLayouts = [
+  'normal',
+  'emblem',
+  'token',
+  'leveler',
+  'split',
+  'planar',
+  'double_faced_token',
+  'flip',
+  'transform',
+  'saga',
+  'host',
+  'augment',
+  'meld',
+  'scheme',
+  'vanguard' 
+];
