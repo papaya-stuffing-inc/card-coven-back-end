3742980c4d6934923fd2a8db7a4cfabacb6fb221
Cosmo Kent <cosmolightfoot@gmail.com>
2019-07-24 17:50:00 -0700

    wrote query add recursive function

--- server.js
+++ server.js
@@ -1,36 +1,31 @@
-// require('dotenv').config();
-const axios = require('axios');
+require('dotenv').config();
 require('./lib/utils/connect')();
-const initializeCardData = require('./lib/services/initialize-card-data');
-const updateCardData = require('./lib/services/update-card-data');
+const Card = require('./lib/models/Card');
+const fetchCardData = require('./lib/services/fetch-card-data');
+const mongoose = require('mongoose');
 const app = require('./lib/app');
-const fs = require('fs');
 const schedule = require('node-schedule');
 
-const cardDataExists = fs.existsSync('./card-data/scryfall/card-data.json');
 const PORT = process.env.PORT || 7891;
 
-function runServer() {
-  app.listen(PORT, () => {
-    //eslint-disable-next-line no-console
-    console.log(`You are listening to smoooth jazz on port ${PORT}`);
-    //eslint-disable-next-line no-unused-vars
-    const scheduleUpdate = schedule.scheduleJob('0 30 16 * * 1,4', () => {
-      //eslint-disable-next-line no-console
-      console.log('Updating card data');
-      updateCardData();
-    });
-  });
-}
 
-if(!cardDataExists) {
+app.listen(PORT, () => {
   //eslint-disable-next-line no-console
-  console.log('No card data exists. Initializing card data now...');
-  initializeCardData(runServer);
-} else {
-  //eslint-disable-next-line no-console
-  console.log('Card Data Exists');
-  runServer();
-}
+  console.log(`You are listening to smoooth jazz on port ${PORT}`);
+  Card.find()
+    .then(res => {
+      console.log('DB', res);
+      if(res.length === 0) fetchCardData(); 
+    });
+    
+  //eslint-disable-next-line no-unused-vars
+  const scheduleUpdate = schedule.scheduleJob('0 30 16 * * 1,4', () => {
+    //eslint-disable-next-line no-console
+    console.log('Updating card data');
+    mongoose.connection.dropCollection('cards');
+    fetchCardData();
+  });
+});
+
 
 
