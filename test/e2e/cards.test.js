407daeccf85cb43ace6c534f24ab122fdc36c2ca
Cosmo Kent <cosmolightfoot@gmail.com>
2019-07-29 22:37:49 -0700

    removed dotenv for heroku

--- test/e2e/cards.test.js
+++ test/e2e/cards.test.js
@@ -1,4 +1,4 @@
-require('dotenv').config();
+// require('dotenv').config();
 const connect = require('../../lib/utils/connect');
 const request = require('supertest');
 const mongoose = require('mongoose');
